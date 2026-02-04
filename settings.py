279d430a119684726978ee93ed72f25fd875d738
jusenglee <fpdlswj81@gmail.com>
2026-02-04 16:59:51 +0900

    ntis_perf_v2 로 변경, 프롬프트 변경

--- settings.py
+++ settings.py
@@ -31,7 +31,7 @@ def _split_csv(value: str | None, default: list[str]) -> list[str]:
 
 RAG_COLLECTION_ALLOWLIST = _split_csv(
     os.getenv("RAG_COLLECTION_ALLOWLIST"),
-    ["ntis_project_v3", "ntis_perf_v3"],
+    ["ntis_project_v3", "ntis_perf_v2"],
 )
 
 # Triton
